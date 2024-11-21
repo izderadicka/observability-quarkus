153faf5ced4019c05190352167a48cbc9f22e0c8
Ivan <ivan.zderadicka@gmail.com>
2024-11-21 18:19:36 +0100

    Add store project

---
+++ store/src/main/java/eu/zderadicka/StoreResource.java
@@ -0,0 +1,16 @@
+package eu.zderadicka;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class StoreResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
